19ab484c3a0d789a42cb0d7e0069ab3d29152aa7
patchaya sonta <gtpatchaya@gmail.com>
2026-02-10 16:04:47 +0700

    update cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 3000;
 
-// บอกให้ Express อ่าน JSON ได้
+app.use(cors());
+
 app.use(express.json());
 app.use(cookieParser());
 
